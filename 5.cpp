f542a3b707ab4dc69ab438508122c69bf1691da1
akashfit2max <36466687+akashfit2max@users.noreply.github.com>
2020-05-05 17:19:05 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>umap;
+        for(int i=0;i<s.length();i++){
+            umap[s[i]]++;
+        }
+       for(int i=0;i<s.length();i++){
+           if(umap[s[i]]==1){
+               return i;
+               break;
+           }
+       }
+        return -1;
+    }
+};
