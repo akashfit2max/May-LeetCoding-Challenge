8fb922c4dd0dda4c8a5523aeb0d050a41b508ee5
akashfit2max <36466687+akashfit2max@users.noreply.github.com>
2020-05-06 17:25:59 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto i:mp){
+            if(i.second>n/2){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
